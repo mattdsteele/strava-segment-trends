410a566a880a39a8e12df107f463e458231d0d92
mattdsteele <orphum@gmail.com>
2020-06-27 10:32:31 -0500

    Add cloud function

---
+++ fn.go
@@ -0,0 +1,33 @@
+package trends
+
+import (
+	"context"
+	"os"
+	"time"
+)
+
+// Message is the payload of a Pub/Sub event. Please refer to the docs for
+// additional information regarding Pub/Sub events.
+type Message struct {
+	Data []byte `json:"data"`
+}
+
+func GetLatestTrends(_ context.Context, _ Message) error {
+	stravaExpiryStr := os.Getenv("STRAVA_EXPIRY")
+	stravaExpiry, _ := time.Parse(time.RFC3339, stravaExpiryStr)
+	s := InitWithRefresh(StravaConfig{
+		AccessToken:  os.Getenv("STRAVA_ACCESS_TOKEN"),
+		RefreshToken: os.Getenv("STRAVA_REFRESH_TOKEN"),
+		Expiry:       stravaExpiry,
+		ClientId:     os.Getenv("STRAVA_CLIENT_ID"),
+		ClientSecret: os.Getenv("STRAVA_CLIENT_SECRET"),
+	})
+	db := InitDb(os.Getenv("FAUNA_SECRET"))
+
+	t := &Trends{
+		Strava: s,
+		Db:     db,
+	}
+	t.Populate()
+	return nil
+}
