2ccc077a8cb5c561e61cd4850ea1309c14511606
mattdsteele <orphum@gmail.com>
2020-06-28 22:22:48 -0500

    A bit more charting

--- frontend/src/charts.js
+++ frontend/src/charts.js
@@ -33,8 +33,37 @@ const example = {
 };
 
 const renderChart = async (data) => {
-  const { spec } = vegaLite.compile(example);
+  const withNow = [
+    ...data,
+    {
+      ts: new Date().toISOString(),
+      efforts: 0,
+    },
+  ];
+  const schema = {
+    $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+    data: {
+      values: withNow.filter((d) => d.efforts >= 0),
+    },
+    mark: 'bar',
+    width: 600,
+    height: 300,
+    encoding: {
+      x: {
+        field: 'ts',
+        timeUnit: 'hours',
+        type: 'temporal',
+      },
+      y: {
+        aggregate: 'sum',
+        field: 'efforts',
+        type: 'quantitative',
+      },
+    },
+  };
+  const { spec } = vegaLite.compile(schema);
   const view = new vega.View(vega.parse(spec), { renderer: 'none' });
-  return await view.toSVG();
+  const svg = await view.toSVG();
+  return `<div class="chart">${svg}</div>`;
 };
 module.exports.renderChart = renderChart;
