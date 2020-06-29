e21230cff95614b80baaf1a8cd3781914d946831
mattdsteele <orphum@gmail.com>
2020-06-28 20:15:58 -0500

    A bit of date filtering

--- frontend/.eleventy.js
+++ frontend/.eleventy.js
@@ -1,7 +1,14 @@
 const { pluralize } = require('journalize');
 const { renderChart } = require('./src/charts');
-// const { renderChart } = require('./src/charts');
+const { ZonedDateTime, ZoneId, DateTimeFormatter } = require('@js-joda/core');
+const { Locale } = require('@js-joda/locale_en-us');
 module.exports = (eleventyConfig) => {
   eleventyConfig.addFilter('pluralize', (value) => pluralize(value));
+  eleventyConfig.addFilter('localTime', (value) => {
+    const z = ZonedDateTime.parse(value)
+      .withZoneSameInstant(ZoneId.of('America/Chicago'))
+      .format(DateTimeFormatter.ofPattern('MM/dd ha').withLocale(Locale.US));
+    return z;
+  });
   eleventyConfig.addNunjucksAsyncShortcode('showchart', renderChart);
 };
