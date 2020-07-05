60234fe28e4efbb3e7d63458d8a470c093a461ad
Matt Steele <orphum@gmail.com>
2020-07-05 08:01:50 -0500

    Update time.js

--- frontend/time.js
+++ frontend/time.js
@@ -3,7 +3,7 @@ const { statsForSegment } = require('./src/charts');
 require('@js-joda/timezone');
 
 (async () => {
-  
+ 
   const [segment] = await checkOrGet('segment-counts');
   console.log(statsForSegment(segment));
 })();
