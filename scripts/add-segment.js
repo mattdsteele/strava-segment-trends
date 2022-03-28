21599e47ee8cd72ba86a0290d43579f17fdff33b
Matt Steele <orphum@gmail.com>
2022-03-28 18:17:14 -0500

    feat: support pasting and getting data

--- scripts/add-segment.js
+++ scripts/add-segment.js
@@ -5,13 +5,37 @@ import { uploadMap } from '../frontend/src/maps.js';
 const db = new Firestore({
   projectId: "secret-strava"
 });
-const {stravaUrl} = await inquirer.prompt([{
+const answers = await inquirer.prompt([{
     type: 'input',
     name: 'stravaUrl',
     message: 'Paste Strava URL:'
+}, {
+    type: 'list',
+    name: 'sport',
+    message: 'Sport type',
+    choices: ['cycling', 'running'],
+    default: 'cycling'
+}, {
+    type: 'input',
+    name: 'weather',
+    message: 'Weather Station Id',
+    default: 'C8198'
+}, {
+    type: 'input',
+    name: 'trailName',
+    message: 'Trail Name'
 }]);
-const results = stravaUrl.match(/segments\/(.*)\w+/)
+const {stravaUrl, sport, weather, trailName} = answers;
+const results = stravaUrl.match(/segments\/(.*)/)
 const segmentId = results[1]
 console.log(segmentId)
 await uploadMap(segmentId);
+const d = {
+    segmentId,
+    sport,
+    trail: trailName,
+    weatherStationId: weather
+}
+const newDoc = await db.collection("segments").doc();
+await newDoc.set(d);
 console.log('uploaded')
