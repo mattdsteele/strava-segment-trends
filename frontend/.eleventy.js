3c72f5ccf4fb8b1633d1041998982a3e51c2593f
mattdsteele <orphum@gmail.com>
2023-04-05 21:20:00 -0500

    Update to 11ty 2.0

--- frontend/.eleventy.js
+++ frontend/.eleventy.js
@@ -27,8 +27,4 @@ module.exports = (eleventyConfig) => {
     const base64 = Buffer.from(svg).toString('base64');
     return `data:image/svg+xml;base64,${base64}`;
   });
-
-  eleventyConfig.setBrowserSyncConfig({
-    ghostMode: false,
-  });
 };
--- frontend/src/weather.js
+++ frontend/src/weather.js
@@ -4,6 +4,5 @@ const EleventyFetch = require('@11ty/eleventy-fetch');
 module.exports.pirateweather = async (lat, lon) => {
   const pirateKey = env['PIRATEWEATHER_KEY'];
   const url = `https://api.pirateweather.net/forecast/${pirateKey}/${lat},${lon}`;
-  console.log(url);
   return await EleventyFetch(url, {duration: '6h', type: 'json'});
 };
